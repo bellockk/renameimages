0420895eb75bec461a1f8c528f09834a0318f823
bellockk <bellockk@segmail.com>
2016-06-01 13:50:33 -0400

    Updating and cleaning

--- lib/renameimages.py
+++ lib/renameimages.py
@@ -6,18 +6,21 @@ import sys
 import time
 import datetime
 
+
 def get_exif_time(fn):
     ret = {}
     try:
         i = IMG.open(fn)
         info = i._getexif()
         for atag, value in info.items():
-            decoded = TAGS.get(atag,atag)
+            decoded = TAGS.get(atag, atag)
             ret[decoded] = value
-        t = datetime.datetime.fromtimestamp(time.mktime(time.strptime(ret['DateTimeOriginal'],'%Y:%m:%d %H:%M:%S')))
+        t = datetime.datetime.fromtimestamp(time.mktime(time.strptime(
+            ret['DateTimeOriginal'], '%Y:%m:%d %H:%M:%S')))
     except:
         return None
     return t
+
 
 def get_file_time(fn):
     """
@@ -36,11 +39,13 @@ def get_file_time(fn):
     t = datetime.datetime.fromtimestamp(os.path.getmtime(fn))
     return t
 
+
 def get_time(fn):
     t = get_exif_time(fn)
     if t:
         return t
     return get_file_time(fn)
+
 
 def get_name(fn):
     str_format = '%Y-%m-%d-%H-%M-%S-%f'
@@ -48,7 +53,8 @@ def get_name(fn):
     t = get_time(fn)
     fext = fn[-4:]
     while True:
-        new = os.path.join('/home','bellockk','Pictures',str(t.year),'%s%s'%(t.strftime(str_format),fext))
+        new = os.path.join('/home', 'bellockk', 'Pictures', str(t.year),
+                           '%s%s' % (t.strftime(str_format), fext))
         if os.path.basename(new) == fn:
             return None
         if not os.path.exists(new):
@@ -56,23 +62,25 @@ def get_name(fn):
         t = t + onemicrosec
     return
 
+
 def add_sec(t):
     return t + datetime.timedelta(seconds=1)
+
 
 def rename_file(fn):
     old = os.path.abspath(fn)
     new = get_name(fn)
     if not new:
-#        print 'Not Moving: %s'%old
         return
-    print 'Moving: %s -> %s'%(old,new)
-    os.renames(old,new)
+    print 'Moving: %s -> %s' % (old, new)
+    os.renames(old, new)
+
 
 def rename_path(pth):
     for root, dirs, files in os.walk(pth):
         for name in files:
-            fn = os.path.join(root,name)
-            if fn[-4:].lower() in ['.jpg','.gif','.bmp','.tif']:
+            fn = os.path.join(root, name)
+            if fn[-4:].lower() in ['.jpg', '.gif', '.bmp', '.tif']:
                 rename_file(fn)
 
 rel_path = sys.argv[1]
